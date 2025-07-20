55e87883c2d7065cca09dfc62702a282ff35c2b8
TANJAKA Jerry Jacques <tanjakajerryjacques@gmail.com>
2025-07-20 10:00:44 +0300

    add : footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,48 @@
-import { useState } from 'react';
-import './index.css'
-import Card from './components/Card.jsx';
-import Header from './components/Header.jsx';
-import SetOffsetButton from './components/setOffsetButton.jsx';
-import NotFoundPage from './components/NotFoundPage.jsx';
-import PokemonDetails from './components/PokemonDetails.jsx';
+/** @format */
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { StrictMode } from 'react';
+import { useState } from "react";
+import Card from "./components/Card.jsx";
+import Header from "./components/Header.jsx";
+import NotFoundPage from "./components/NotFoundPage.jsx";
+import PokemonDetails from "./components/PokemonDetails.jsx";
+import SetOffsetButton from "./components/setOffsetButton.jsx";
+import "./index.css";
 
+import { StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-  const [offset, setOffset] = useState(0);
-  const [search, setSearch] = useState("")
+	const [offset, setOffset] = useState(0);
+	const [search, setSearch] = useState("");
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <>
-        <Header search={search} setSearch={setSearch} />
-        <Card offset={offset} search={search} />
-        <SetOffsetButton setOffset={setOffset} />
-      </>
-    },
-    {
-      path: "*", 
-      element: <NotFoundPage />
-    },
-    {
-      path: "/details/:nameUrl",
-      element: <PokemonDetails/>
-    }
-  ])
+	const router = createBrowserRouter([
+		{
+			path: "/",
+			element: (
+				<>
+					<Header search={search} setSearch={setSearch} />
+					<Card offset={offset} search={search} />
+					<SetOffsetButton setOffset={setOffset} />
+					<Footer />
+				</>
+			),
+		},
+		{
+			path: "*",
+			element: <NotFoundPage />,
+		},
+		{
+			path: "/details/:nameUrl",
+			element: <PokemonDetails />,
+		},
+	]);
 
-  return (
-    <StrictMode>
-      <RouterProvider router={router} />
-
-    </StrictMode>
-  )
+	return (
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>
+	);
 }
 
-export default App
+export default App;
