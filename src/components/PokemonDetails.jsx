19f60a712e3c09eaa6e88d92db036737642f6654
Ilo <hei.iloniaina@gmail.com>
2025-07-24 17:29:45 +0300

    refactor : refactor: made pokemon details into a reusable component

--- src/components/PokemonDetails.jsx
+++ src/components/PokemonDetails.jsx
@@ -3,8 +3,7 @@ import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Loading from "./Loading.jsx";
 import Error from "./Error.jsx";
-
-import getTypeColorClass from "../script/getColorType.js";
+import PokeDetails from "./PokeDetails.jsx";
 
 
 export default function PokemonDetails() {
@@ -81,48 +80,20 @@ export default function PokemonDetails() {
             <Link to={"/"}>
                 <i className="bx bxs-x-circle absolute bg-white rounded-full text-primary-red text-4xl"></i>
             </Link>
-            <section className="py-20 px-8 items-center justify-between cursor-pointer gap-10 flex flex-col sm:flex-row inset-0 backdrop-blur-3xl bg-gray-400/30 w-[50vw] rounded-4xl self-center shadow-2xl shadow-gray-800 mx-auto">
-                <div className="flex-1/2">
-                    <img src={result.image} className="mx-auto w-40 h-40" />
-                    <h1 className="text-3xl font-bold capitalize text-center my-3">{result.name}</h1>
-                    <center>
-                        <h3 className="bg-primary-yellow w-fit px-4 rounded-2xl font-bold mb-2 ">#{result.id}</h3>
-                    </center>
-                    <p className="text-sm italic text-center text-black mb-4">{result.description}</p>
-                </div><div className="flex-1/2  border-l-2 pl-10">
-                    <p className="my-1 flex flex-wrap gap-2 items-center inset-0 backdrop-blur-2xl bg-white/20 rounded-2xl px-2.5 py-0.5">
-                        <strong className="flex items-center text-nowrap">
-                            <i className='bx mr-1 bxs-filter'></i>Type:
-                        </strong>
-                        {result.types.map((type) => (
-                            <span
-                                key={type}
-                                className={`${getTypeColorClass(type)} px-2 py-0.5 rounded-full text-xs font-bold capitalize`}
-                            >
-                                {type}
-                            </span>
-                        ))}
-                    </p>
-
-                    <p className="my-1 flex row gap-2 inset-0 backdrop-blur-2xl bg-white/20  rounded-2xl px-2.5 py-0.5 ">
-                        <strong className="flex row items-center text-nowrap"><i className='bxr mr-1 bx-area'></i> Region:</strong> {result.region}
-                    </p>
-                    <p className="my-1 flex row gap-2 inset-0 backdrop-blur-2xl bg-white/20  rounded-2xl px-2.5 py-0.5 ">
-                        <strong className="flex row items-center text-nowrap"><i className='bxr mr-1  bx-ruler'></i>Height:</strong> {result.height / 10} M
-                    </p>
-                    <p className="my-1 flex row gap-2 inset-0 backdrop-blur-2xl bg-white/20  rounded-2xl px-2.5 py-0.5 ">
-                        <strong className="flex row items-center text-nowrap"><i className='bxr mr-1  bx-dumbbell'></i>Weight:</strong> {result.weight / 10} KG
-                    </p>
-                    <p className="my-1 flex row gap-2 inset-0 backdrop-blur-2xl bg-white/20  rounded-2xl px-2.5 py-0.5 ">
-                        <strong className="flex row items-center text-nowrap"><i className='bxr mr-1  bxs-chess-knight'></i> Attacks:</strong> {result.attack}
-                    </p>
-                    <p className="my-1 flex row gap-2 inset-0 backdrop-blur-2xl bg-white/20  rounded-2xl px-2.5 py-0.5 mb-3 text-nowrap ">
-                        <strong className="flex row items-center text-nowrap"><i class='bxr mr-1 bxs-trending-up'  ></i> Growth Rate:</strong> {result.growthRate}
-                    </p>
-                    <p className="my-1  "><strong>Location:</strong> {result.location.join(", ")}</p>
-                    <p className="my-1  "><strong>Évolution:</strong> {result.evolution.join(" → ")}</p>
-                </div>
-            </section>
+            <PokeDetails
+                image={result.image}
+                name={result.name}
+                id={result.id}
+                description={result.description}
+                types={result.typs}
+                region={result.region}
+                height={result.height}
+                weight={result.weight}
+                attack={result.attack}
+                growthRate={result.description}
+                location={result.location}
+                evolution={result.evolution}
+            />
         </div>
     );
 }
